b814116b89166584bafcd33ad16e1f68a77ae16d
Ali Najafizadeh <a.najafizadeh@gmail.com>
2015-12-09 10:06:23 -0500

    refactoring the whole thing

--- example/Sample2/index.ios.js
+++ example/Sample2/index.ios.js
@@ -33,23 +33,4 @@ var Sample2 = React.createClass({
   }
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
 AppRegistry.registerComponent('Sample2', () => Sample2);
