6d996ec2836f4ad27334a6ac5ff1ed522d1c344e
Ali Najafizadeh <a.najafizadeh@gmail.com>
2016-02-10 14:05:14 -0500

    updated the example to support both ios and android

---
+++ examples/Sample2/app.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var WebViewBridge = require('react-native-webview-bridge');
+
+const injectScript = `
+  (function () {
+    if (WebViewBridge) {
+
+      WebViewBridge.onMessage = function (message) {
+        alert('got a message from Native: ' + message);
+
+        WebViewBridge.send("message from webview");
+      };
+
+    }
+  }());
+`;
+
+var Sample2 = React.createClass({
+  componentDidMount() {
+    setTimeout(() => {
+      this.refs.webviewbridge.sendToBridge("hahaha");
+    }, 5000);
+  },
+  onBridgeMessage: function (message) {
+    console.log(message);
+  },
+  render: function() {
+    return (
+      <WebViewBridge
+        ref="webviewbridge"
+        onBridgeMessage={this.onBridgeMessage}
+        injectedJavaScript={injectScript}
+        onBridgeMessage={(message) => {
+          console.log(message);
+        }}
+        url={"http://google.com"}/>
+    );
+  }
+});
+
+module.exports = Sample2;
