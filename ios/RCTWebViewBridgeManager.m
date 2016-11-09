0ac0d77e481d282632f18c917b903d891ce1c604
Guilherme Bruzzi <guibruzzi@gmail.com>
2016-11-09 11:33:07 -0200

    Fix iOS imports on RN0.40+ and replace UIWebview with WkWebview
    No more deprecated webview on iOS and let people use webviewbridge API with RN 0.40+
    
    Commit messages:
    
    Fix iOS imports on RN 0.40+
    
    Replacing UIWebview with WkWebview
    
    Fix WKWebview Bridge
    
    Remove debug informations
    
    Fix injectedJavaScript property

--- ios/RCTWebViewBridgeManager.m
+++ ios/RCTWebViewBridgeManager.m
@@ -11,11 +11,12 @@
  */
 
 #import "RCTWebViewBridgeManager.h"
-
-#import "RCTBridge.h"
-#import "RCTUIManager.h"
 #import "RCTWebViewBridge.h"
-#import "UIView+React.h"
+
+#import <React/RCTBridge.h>
+#import <React/RCTUIManager.h>
+
+#import <React/UIView+React.h>
 
 @interface RCTWebViewBridgeManager () <RCTWebViewBridgeDelegate>
 
