960167a7910a0be06b964141bbba89048e59ea72
Roman Volosovskyi <rasom@ukr.net>
2016-12-26 10:21:08 +0200

    fix WebViewBridge js interface

--- android/src/main/java/com/github/alinz/reactnativewebviewbridge/WebViewBridgeManager.java
+++ android/src/main/java/com/github/alinz/reactnativewebviewbridge/WebViewBridgeManager.java
@@ -119,6 +119,7 @@ public class WebViewBridgeManager extends ReactWebViewManager {
         }
 
         webView.setWebChromeClient(new ReactWebChromeClient());
+        webView.addJavascriptInterface(new JavascriptBridge(webView), "WebViewBridge");
 
         return webView;
     }
