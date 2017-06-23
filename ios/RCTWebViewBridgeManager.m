28d97d60a9fdddea8a9890f3ea68e25fde7b3025
Ronnie Ren <renzhaosong@niwodai.net>
2017-06-23 18:31:23 +0800

    fix compiling error after upgrading latest react native library

--- ios/RCTWebViewBridgeManager.m
+++ ios/RCTWebViewBridgeManager.m
@@ -12,10 +12,10 @@
 
 #import "RCTWebViewBridgeManager.h"
 
-#import "RCTBridge.h"
-#import "RCTUIManager.h"
+#import <React/RCTBridge.h>
+#import <React/RCTUIManager.h>
 #import "RCTWebViewBridge.h"
-#import "UIView+React.h"
+#import <React/UIView+React.h>
 
 @interface RCTWebViewBridgeManager () <RCTWebViewBridgeDelegate>
 
