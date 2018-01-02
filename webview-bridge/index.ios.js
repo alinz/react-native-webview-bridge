47f19a43b3d6e84ba610d06670f84d951273e6fb
Nagender Singh <nagendersinghh@gmail.com>
2018-01-02 15:21:34 +0530

    Use createReactClass instead of React.createClass

--- webview-bridge/index.ios.js
+++ webview-bridge/index.ios.js
@@ -13,8 +13,7 @@
  * @flow
  */
 'use strict'
-
-var React = require('react')
+var createReactClass = require('create-react-class')
 var ReactNative = require('react-native')
 var invariant = require('invariant')
 var keyMirror = require('keymirror')
@@ -90,7 +89,7 @@ var defaultRenderError = (errorDomain, errorCode, errorDesc) => (
 /**
  * Renders a native WebView.
  */
-var WebViewBridge = React.createClass({
+var WebViewBridge = createReactClass({
   statics: {
     JSNavigationScheme: JSNavigationScheme,
     NavigationType: NavigationType
